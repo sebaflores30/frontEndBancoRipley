ea3a754f496cc9f06d0c188c52446c0679168af1
sebaflores30 <seba_rfc@hotmail.com>
2021-07-15 01:40:16 -0400

    Âcommit completo

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/frontEndBancoRipley'));
+app.use(express.static('./dist/prubaBancaRipley'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/frontEndBancoRipley/'}),
+    res.sendFile('index.html', {root: 'dist/prubaBancaRipley/'}),
 );
 
 // Start the app by listening on the default Heroku port
