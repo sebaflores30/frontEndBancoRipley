8db3fb004c77569d4e045d3af0fb17858c739ba1
sebaflores30 <seba_rfc@hotmail.com>
2021-07-14 20:45:29 -0400

    Âcommit completo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  urlApi: 'https://banco-ripley.herokuapp.com',
+  historialApi: '/api/historial',
+  destinatarioApi: '/api/destinatarios',
+  loginApi: '/api/clientes/177011193/hola1234',
+  bancosApi: 'https://bast.dev/api/banks.php',
+  transferencia: '/api/guardaTransaccion'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@
 export const environment = {
   production: false,
   urlApi: 'https://banco-ripley.herokuapp.com',
-  //urlApi: 'http://localhost:3000',
   historialApi: '/api/historial',
   destinatarioApi: '/api/destinatarios',
   loginApi: '/api/clientes/177011193/hola1234',
